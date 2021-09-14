07e53b9f9486b4eaf1431475a7cbf03fe24d9837
mADoTM <kon4ita228@gmail.com>
2021-09-14 23:05:44 +0300

    Add Main.java with task's solve

--- src/ru/vsu/cs/dolzhenkoms/Main.java
+++ src/ru/vsu/cs/dolzhenkoms/Main.java
@@ -1,20 +1,28 @@
 package ru.vsu.cs.dolzhenkoms;
 
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getCountGroups(9,7)); // 5
-        System.out.println(getCountGroups(2,1)); // 1
-        System.out.println(getCountGroups(30,0)); // 0
-        System.out.println(getCountGroups(20,15)); // 11
-        System.out.println(getCountGroups(45, 10)); // 10
+
+        int n = readNumber();
+        int m = readNumber();
+        int countGroups = getCountGroups(n,m);
+
+        printCountGroups(n, m, countGroups);
     }
 
+    private static int readNumber() {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        return num;
+    }
     private static int getCountGroups(int N, int M) {
         int countGroups = 0;
 
-        while(N > 0 && M > 0)
-        {
+        while(N > 0 && M > 0){
             N--; M--;
             if(N > 0  || M > 0){
                 if(N >= M){
@@ -28,5 +36,13 @@ public class Main {
         }
 
         return countGroups;
+    }
+    private static void printCountGroups(int n, int m, int countGroups) {
+        if(n < 0 || m < 0) {
+            System.out.println("Wrong number of boys or girls.");
+        }
+        else {
+            System.out.printf("You can make %d groups from %d boys and %d girls", countGroups, n, m);
+        }
     }
 }
