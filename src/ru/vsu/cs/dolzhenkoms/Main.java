0bd85a0e7efc4e0809016b21c9bfc36e363f26cc
mADoTM <kon4ita228@gmail.com>
2021-09-16 23:41:04 +0300

    Add custom readNumber() and make code more clean

--- src/ru/vsu/cs/dolzhenkoms/Main.java
+++ src/ru/vsu/cs/dolzhenkoms/Main.java
@@ -6,30 +6,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int countBoys = readNumber("boys");
+        int countGirls = readNumber("girls");
+        int countGroups = getCountGroups(countBoys, countGirls);
 
-        int n = readNumber();
-        int m = readNumber();
-        int countGroups = getCountGroups(n,m);
-
-        printCountGroups(n, m, countGroups);
+        printCountGroups(countBoys, countGirls, countGroups);
     }
 
-    private static int readNumber() {
+    private static int readNumber(String childrenType) {
+        System.out.printf("Write count of  %s - ", childrenType);
         Scanner scn = new Scanner(System.in);
         int num = scn.nextInt();
+
+        if(num < 0) {
+            System.out.println("Entered wrong data. Please retry...");
+            System.exit(0);
+        }
+
         return num;
     }
-    private static int getCountGroups(int N, int M) {
+
+    private static int getCountGroups(int countBoys, int countGirls) {
         int countGroups = 0;
 
-        while(N > 0 && M > 0){
-            N--; M--;
-            if(N > 0  || M > 0){
-                if(N >= M){
-                    N--;
+        while(countBoys > 0 && countGirls > 0){
+            countBoys--; countGirls--;
+            if(countBoys > 0  || countGirls > 0){
+                if(countBoys >= countGirls){
+                    countBoys--;
                 }
                 else{
-                    M--;
+                    countGirls--;
                 }
                 countGroups++;
             }
@@ -37,12 +44,13 @@ public class Main {
 
         return countGroups;
     }
-    private static void printCountGroups(int n, int m, int countGroups) {
-        if(n < 0 || m < 0) {
-            System.out.println("Wrong number of boys or girls.");
+
+    private static void printCountGroups(int countBoys, int countGirls, int countGroups) {
+        if(countGroups == 0) {
+            System.out.println("You can't make no one groups.");
         }
         else {
-            System.out.printf("You can make %d groups from %d boys and %d girls", countGroups, n, m);
+            System.out.printf("You can make %d groups from %d boys and %d girls", countGroups, countBoys, countGirls);
         }
     }
 }
